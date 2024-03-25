cf0bc3a1e9d75c038424a222d809c77bc1478104
Taylon Kalel <taylon.kalel@gmail.com>
2024-03-25 18:25:13 -0300

    first commit

---
+++ src/tweets/entities/tweet.entity.ts
@@ -0,0 +1,13 @@
+export type TweetProps = {
+	content: string;
+	screen_name: string;
+}
+
+export class Tweet {
+	content: string;
+	screen_name: string;
+
+	constructor(props: TweetProps) {		
+		Object.assign(this, props)
+	}
+}
