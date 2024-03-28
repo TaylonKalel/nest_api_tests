c3954cc9cc87b565d12b217361de45eaac09e349
Taylon Kalel <taylon.kalel@gmail.com>
2024-03-28 12:43:33 -0300

    .

--- src/tweets/entities/tweet.entity.spec.ts
+++ src/tweets/entities/tweet.entity.spec.ts
@@ -1,22 +1,40 @@
+import { describe } from "node:test";
 import { Tweet, TweetSchema } from "./tweet.entity";
 import mongoose from 'mongoose';
 
-describe('Tweet Tests', () =>{
-	it('should create a tweet', () =>{
-		const tweet = new Tweet({
-			content: 'Hello World',
-			screen_name: 'Luiz Carlos'
+describe('Tweet Tests', () => {
+	describe('Local', ()=>{
+		it('should create a tweet', () => {
+			const tweet = new Tweet({
+				content: 'Hello World',
+				screen_name: 'Luiz Carlos'
+			});
+	
+			expect(tweet.content).toBe('Hello World');
+			expect(tweet.screen_name).toBe('Luiz Carlos');
 		});
+	})
+	describe('Using MongoDB', () => {
+		let conn: mongoose.Mongoose;
+		beforeEach(async () => {
+			conn = await mongoose.connect('mongodb://root:root@localhost:27017/tweets_test?authSource=admin');
+		})
+		afterEach(async ()=> {
+			await conn.disconnect();
+		})
+		it('create a tweet document', async () => {
+			const TweetModel = conn.model('Tweet', TweetSchema);
+			const tweetModel = new TweetModel({
+				content: 'Hello World',
+				screen_name: 'Luiz Carlos'
+			});
+			await tweetModel.save();
+			const tweetCreated = await TweetModel.findById(tweetModel._id);
+			expect(tweetCreated.content).toBe('Hello World');
+			expect(tweetCreated.screen_name).toBe('Luis Carlos');
+			await conn.disconnect();
+		});
+	})
 
-		expect(tweet.content).toBe('Hello World');
-		expect(tweet.screen_name).toBe('Luiz Carlos');
-	});
-	it('create a tweet document', async () =>{
-		const conn = await mongoose.connect('mongodb://root:root@db:27017/tweets_test');
-		const TweetModel = conn.model('Tweet', TweetSchema);
-		const tweetModel = new TweetModel({content: 'Hello World',
-		screen_name: 'Luiz Carlos'});
-		await tweetModel.save();
-	});
 
 })
